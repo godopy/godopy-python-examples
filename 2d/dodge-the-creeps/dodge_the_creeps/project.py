291bc23aba3dc04f93238ad6b990ea1986a7c886
Ivan Hilaire <ivhilaire@gmail.com>
2020-02-03 23:05:20 +0300

    Update DtC/project.py

--- 2d/dodge-the-creeps/dodge_the_creeps/project.py
+++ 2d/dodge-the-creeps/dodge_the_creeps/project.py
@@ -7,9 +7,26 @@ GODOT_PROJECT = BASE_DIR / 'gd' / 'dodge_the_creeps'
 PYTHON_PACKAGE = 'dodge_the_creeps'
 GDNATIVE_LIBRARY = 'dodge-the-creeps.gdnlib'
 
+NAME = 'Dodge the Creeps'
+MAIN_SCENE = 'res://Main.tscn'
+ICON = 'res://icon.png'
+
+WINDOW_SIZE = (480, 720)
+
+
 DEVELOPMENT_PATH = BASE_DIR
 
 NATIVESCRIPT_EXTENSIONS = [
     'scripts/TermShell',
     'Player'
 ]
+
+INPUT = {
+    'move_left': {
+        'dead_zone': 0.5,
+        'events': []  # TODO
+    },
+    # TODO
+}
+
+DEFAULT_ENVIRONMENT = 'res://default_env.tres'
