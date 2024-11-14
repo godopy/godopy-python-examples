83396b26834254d3e3c0c26e7f2fecb517f702ce
Ivan Hilaire <ivhilaire@gmail.com>
2024-11-14 21:43:50 +0400

    Update extension example

--- 2d/gdexample-ext/lib/gdexample.py
+++ 2d/gdexample-ext/lib/gdexample.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-from godot import PropertyHint
 from godot.core import GDCLASS
+from godot.enums import PropertyHint
 from godot.types import Vector2
-from godot.classdb import Sprite2D, ClassDB
+from godot.classdb import Sprite2D
 from godot.property_info import PropertyInfo
 
 
@@ -13,12 +13,12 @@ class GDExample:
 
     @classmethod
     def _bind_methods(cls):
-        ClassDB.bind_method(cls.set_amplitude)
-        ClassDB.bind_method(cls.get_amplitude)
+        cls.bind_method(cls.set_amplitude)
+        cls.bind_method(cls.get_amplitude)
         cls.add_property(PropertyInfo(float, 'amplitude'), 'set_amplitude', 'get_amplitude')
 
-        ClassDB.bind_method(cls.set_speed)
-        ClassDB.bind_method(cls.get_speed)
+        cls.bind_method(cls.set_speed)
+        cls.bind_method(cls.get_speed)
         speed = PropertyInfo(float, 'speed', PropertyHint.PROPERTY_HINT_RANGE, '0,20,0.01')
         cls.add_property(speed, 'set_speed', 'get_speed')
 
